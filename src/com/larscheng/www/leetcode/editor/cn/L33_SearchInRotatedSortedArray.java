d6e82ec2abf041d749d03b78c429383134379214
larscheng <larscheng@foxmail.com>
2024-03-22 14:01:07 +0800

    #131

--- src/com/larscheng/www/leetcode/editor/cn/L33_SearchInRotatedSortedArray.java
+++ src/com/larscheng/www/leetcode/editor/cn/L33_SearchInRotatedSortedArray.java
@@ -63,6 +63,7 @@ class Solution {
         // 如果中间的数小于最右边的数，则右半段是有序的，
         // 若中间数大于最右边数，则左半段是有序的，
         // 我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
+        // O(logn)/O(1)
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             //int mid = (left + right) / 2;
