f26afb85e27b98bb00cb47b446a1bc8e9a4cdc59
larscheng <larscheng@foxmail.com>
2024-03-30 11:32:13 +0800

    #141

--- src/com/larscheng/www/leetcode/editor/cn/L2_AddTwoNumbers.java
+++ src/com/larscheng/www/leetcode/editor/cn/L2_AddTwoNumbers.java
@@ -61,39 +61,65 @@ public class L2_AddTwoNumbers{
     }
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-
+      //递归，递归计算两节点值
+    //O(n)/O(n)
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
-        // 临时数据
-        ListNode temp = result;
-        int carry = 0;
-        while (l1!=null||l2!=null){
-            int a=0,b=0;
-            if (l1!=null){
-                a = l1.val;
-                l1 = l1.next;
-            }
-            if (l2!=null){
-                b = l2.val;
-                l2 = l2.next;
-            }
+        return addTwo(l1, l2, 0);
+    }
 
-            int sum = carry+a+b;
-            //进位
-            carry = sum/10;
-            //位数
-            sum = sum%10;
-            temp.next = new ListNode(sum);
-            temp = temp.next;
+    private ListNode addTwo(ListNode l1, ListNode l2, int carry) {
+        if (l1==null&&l2==null){
+            return carry == 0 ? null : new ListNode(carry);
         }
-        if (carry>=1){
-            temp.next = new ListNode(carry);
+        if (l1==null){
+            //默认l1为基准长链表，如果l1比l2短，则交换以下
+            l1=l2;
+            l2=null;
         }
+        carry += l1.val + (l2 == null ? 0 : l2.val);
+        //修改l1链表值，最终返回链表l1
+        l1.val = carry%10;
+        //递归计算剩余节点
+        l1.next = addTwo(l1.next,(l2==null?null:l2.next),carry/10);
 
-        return result.next;
+        return l1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    class Solution1 {
 
+        //双指针迭代两个链表的节点，加和后计算位数值和进位值
+        //O(n)/O(1)
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode result = new ListNode();
+            // 临时数据
+            ListNode temp = result;
+            int carry = 0;
+            while (l1!=null||l2!=null){
+                int a=0,b=0;
+                if (l1!=null){
+                    a = l1.val;
+                    l1 = l1.next;
+                }
+                if (l2!=null){
+                    b = l2.val;
+                    l2 = l2.next;
+                }
+
+                int sum = carry+a+b;
+                //进位
+                carry = sum/10;
+                //位数
+                sum = sum%10;
+                temp.next = new ListNode(sum);
+                temp = temp.next;
+            }
+            if (carry>=1){
+                temp.next = new ListNode(carry);
+            }
+
+            return result.next;
+        }
+    }
 }
