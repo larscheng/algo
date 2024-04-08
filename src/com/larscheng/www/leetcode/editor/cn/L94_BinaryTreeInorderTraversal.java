94d0707fda5a286e5a55003089199c3e413408b2
larscheng <larscheng@foxmail.com>
2024-04-08 13:36:10 +0800

    #147

--- src/com/larscheng/www/leetcode/editor/cn/L94_BinaryTreeInorderTraversal.java
+++ src/com/larscheng/www/leetcode/editor/cn/L94_BinaryTreeInorderTraversal.java
@@ -43,6 +43,8 @@ package com.larscheng.www.leetcode.editor.cn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Stack;
 
 public class L94_BinaryTreeInorderTraversal{
       
@@ -55,14 +57,7 @@ public class L94_BinaryTreeInorderTraversal{
                                new L94_BinaryTreeInorderTraversal.TreeNode(7,null,null)
                        )
                );
-      L94_BinaryTreeInorderTraversal.TreeNode node1 = new L94_BinaryTreeInorderTraversal.TreeNode(1,
-                       null,
-                       new L94_BinaryTreeInorderTraversal.TreeNode(2,
-                               new L94_BinaryTreeInorderTraversal.TreeNode(3,null,null),
-                               null
-                       )
-               );
-      solution.inorderTraversal(node1);
+      System.out.println(solution.inorderTraversal(node));
   }
  public static class TreeNode {
      int val;
@@ -94,23 +89,84 @@ public class L94_BinaryTreeInorderTraversal{
  */
 class Solution {
     //O(n)/O(n)
-    //递归中序遍历
+    //颜色标记法，
+    // 1、根节点先入栈，标记位白色
+    // 2、弹出栈顶元素和其对应的颜色标记，跳过空值，
+    //      如果为白色：将栈顶元素按照右子树（白色），当前节点（灰色），左子树（白色）的顺序入栈
+    //      如果为灰色：记录节点值
+    // 3、循环操作2
+    // 这种栈结构中，最先出栈的必定是左子树，最先记录的节点必定是左叶子节点
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        inorder(root, res);
+        Stack<Object> stack = new Stack<>();
+        if (root==null){
+            return res;
+        }
+        stack.push("white");
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            TreeNode node = (TreeNode) stack.pop();
+            String color = (String) stack.pop();
+            if (node == null){
+                continue;
+            }
+            if (color.equals("white")){
+                stack.push("white");
+                stack.push(node.right);
+                stack.push("gray");
+                stack.push(node);
+                stack.push("white");
+                stack.push(node.left);
+            }else {
+                res.add(node.val);
+            }
+        }
         return res;
     }
 
-    private void inorder(TreeNode root, List<Integer> res) {
-        if (root == null) {
-            return;
-        }
-        inorder(root.left, res);
-        res.add(root.val);
-        inorder(root.right, res);
-    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+
+    class Solution3 {
+        //O(n)/O(n)
+        //迭代中序遍历，手动记录栈
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+
+            while (root != null || !stack.isEmpty()) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                //取出栈中的左子树
+                root = stack.pop();
+                res.add(root.val);
+                root = root.right;
+            }
+            return res;
+        }
+
+    }
+    class Solution2 {
+        //O(n)/O(n)
+        //递归中序遍历
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            inorder(root, res);
+            return res;
+        }
+
+        private void inorder(TreeNode root, List<Integer> res) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left, res);
+            res.add(root.val);
+            inorder(root.right, res);
+        }
+    }
 class Solution1 {
     //左节点+根节点+右节点
     //addall 空间复杂度高O(n^2)
