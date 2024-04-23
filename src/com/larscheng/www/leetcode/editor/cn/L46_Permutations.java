776e582aa08e812b897710db90eb2c8e6e29106e
larscheng <larscheng@foxmail.com>
2024-04-23 11:15:07 +0800

    #162

---
+++ src/com/larscheng/www/leetcode/editor/cn/L46_Permutations.java
@@ -0,0 +1,95 @@
+//给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1]
+//输出：[[0,1],[1,0]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1]
+//输出：[[1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 6 
+// -10 <= nums[i] <= 10 
+// nums 中的所有整数 互不相同 
+// 
+//
+// Related Topics 数组 回溯 👍 2873 👎 0
+
+package com.larscheng.www.leetcode.editor.cn;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L46_Permutations{
+      
+  public static void main(String[] args) {
+       Solution solution = new L46_Permutations().new Solution();
+       
+  }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+      //回溯
+      // 声明状态变量used来记录元素的使用状态，depth为当前收集深度
+      // 回溯终止条件：已收集到需要的数字，denpth==length时
+    // O(n*n!)/ O(n*n!)
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length==0){
+            return res;
+        }
+        boolean[] used = new boolean[nums.length];
+        List<Integer> path  = new ArrayList<>();
+
+        dfs(nums,nums.length,0,used,path,res);
+
+        return res;
+
+    }
+
+    private void dfs(int[] nums, int length, int depth, boolean[] used, List<Integer> path, List<List<Integer>> res) {
+        if (depth==length){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]){
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+
+            dfs(nums,length,depth+1,used,path,res);
+
+            used[i] = false;
+            path.remove(path.size()-1);
+
+        }
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
