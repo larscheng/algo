5e915413bdf13a3c13b24726fd71da1ccf0341d8
larscheng <larscheng@foxmail.com>
2024-05-11 13:42:35 +0800

    #55

--- src/com/larscheng/www/leetcode/editor/cn/L198_HouseRobber.java
+++ src/com/larscheng/www/leetcode/editor/cn/L198_HouseRobber.java
@@ -62,6 +62,22 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
+        int[] dp = new int[nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= nums.length; i++) {
+            //dp[2] = max(dp[1],dp[0]+nums[1])
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[nums.length];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class Solution1 {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
         int cur = 0;
         int pre = 0;
         for (int num : nums) {
@@ -72,7 +88,5 @@ class Solution {
         return cur;
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
-
 
 }
