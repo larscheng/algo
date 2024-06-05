de60be70ec10c9babff565d9c54c25943e9b3ff0
larscheng <larscheng@foxmail.com>
2024-06-05 10:39:11 +0800

    add 25

--- src/com/larscheng/www/leetcode/editor/cn/L25_ReverseNodesInKGroup.java
+++ src/com/larscheng/www/leetcode/editor/cn/L25_ReverseNodesInKGroup.java
@@ -48,6 +48,7 @@ public class L25_ReverseNodesInKGroup{
   public static void main(String[] args) {
        Solution solution = new L25_ReverseNodesInKGroup().new Solution();
       ListNode a = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
+//      ListNode a = new ListNode(1,new ListNode(2,null));
 //      solution.reverseKGroup(a,2);
       solution.reverseKGroup(a,3);
 //      solution.reverseKGroup(new ListNode(1,new ListNode(2)),2);
@@ -71,46 +72,85 @@ public static class ListNode {
  * }
  */
 class Solution {
-    //迭代，统计总节点数，将节点分k个一组进行迭代，对单个组进行反转
-    //反转时记录尾节点，用于连接下一组反转后的链表
-    //最后将没有参与分组反转的节点连在尾部
-    //O(n)/O(1)
+    /**
+     * 按k个1组，进行反转，递归找每个组，然后重复反转逻辑
+     * 不足k个，不用反转
+     */
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode cur = head;
-        int length = 0;
-        while (cur!=null){
-            cur = cur.next;
-            length++;
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode temp  = dummy;
-        cur = head;
-        for (int i = 0; i+k <= length ; i+=k) {
-            ListNode pre = null;
-            ListNode tail = null;
-            for (int j = 0; j < k; j++) {
-                if (j==0){
-                    tail = cur;
-                }
-                //保留原始结构
-                ListNode next = cur.next;
-                //反转指向
-                cur.next = pre;
-                //继续遍历
-                pre = cur;
-                cur = next;
+        //找每1组的左右边界
+        ListNode a=head,b=head;
 
+        for (int i = 0; i < k; i++) {
+            if (b==null) {
+                //不足k个元素，不用反转直接返回
+                return head;
             }
-            temp.next = pre;
-            temp = tail ;
+            b = b.next;
         }
-        if (cur!=null){
-            temp.next = cur;
+        //组内反转，到b停止反转
+        ListNode newHead = reverse(a,b);
+        //反转后a为最后一个节点，其他的组接在a.next
+        a.next = reverseKGroup(b, k);
+        //返回头节点
+        return newHead;
+    }
+
+    //迭代反转
+    private ListNode reverse(ListNode a, ListNode b) {
+        ListNode currNode = a;
+        ListNode pre = null;
+        while (currNode!=b) {
+            ListNode next =  currNode.next;
+            //反转
+            currNode.next = pre;
+            //更新pre
+            pre = currNode;
+            //继续遍历
+            currNode = next;
         }
-        return dummy.next;
+        return pre;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    class Solution1 {
+        //迭代，统计总节点数，将节点分k个一组进行迭代，对单个组进行反转
+        //反转时记录尾节点，用于连接下一组反转后的链表
+        //最后将没有参与分组反转的节点连在尾部
+        //O(n)/O(1)
+        public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode cur = head;
+            int length = 0;
+            while (cur!=null){
+                cur = cur.next;
+                length++;
+            }
+            ListNode dummy = new ListNode(0);
+            ListNode temp  = dummy;
+            cur = head;
+            for (int i = 0; i+k <= length ; i+=k) {
+                ListNode pre = null;
+                ListNode tail = null;
+                for (int j = 0; j < k; j++) {
+                    if (j==0){
+                        tail = cur;
+                    }
+                    //保留原始结构
+                    ListNode next = cur.next;
+                    //反转指向
+                    cur.next = pre;
+                    //继续遍历
+                    pre = cur;
+                    cur = next;
 
+                }
+                temp.next = pre;
+                temp = tail ;
+            }
+            if (cur!=null){
+                temp.next = cur;
+            }
+            return dummy.next;
+        }
+    }
 }
