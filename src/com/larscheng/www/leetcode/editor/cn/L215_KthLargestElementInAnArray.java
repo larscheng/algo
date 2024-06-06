75d4eb303cf2910a9bc391644624c988c6f71a58
larscheng <larscheng@foxmail.com>
2024-06-06 15:10:03 +0800

    update 215

--- src/com/larscheng/www/leetcode/editor/cn/L215_KthLargestElementInAnArray.java
+++ src/com/larscheng/www/leetcode/editor/cn/L215_KthLargestElementInAnArray.java
@@ -33,8 +33,8 @@
 package com.larscheng.www.leetcode.editor.cn;
 
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class L215_KthLargestElementInAnArray{
       
@@ -45,20 +45,72 @@ public class L215_KthLargestElementInAnArray{
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    /**
+     * 快速排序
+     * https://blog.csdn.net/qq_61422622/article/details/131667258
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
     public int findKthLargest(int[] nums, int k) {
-        //大顶堆
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o1-o2);
+        List<Integer> num = Arrays.stream(nums).boxed().collect(Collectors.toList());
 
+        return quickSort(num,k);
+    }
+
+
+    private int quickSort(List<Integer> num, int k) {
+        //随机一个下标位置作为标记位
+        //int flag = num.get(new Random().nextInt(num.size()));
+        Random rand = new Random();
+        int flag = num.get(rand.nextInt(num.size()));
+
+        List<Integer> small = new ArrayList<>();
+        List<Integer> bigger = new ArrayList<>();
+        List<Integer> equls = new ArrayList<>();
+
+        for (Integer integer : num) {
+            if (integer>flag) {
+                bigger.add(integer);
+            }else if (integer<flag) {
+                small.add(integer);
+            }else{
+                equls.add(integer);
+            }
+        }
+
+        if (k<=bigger.size()) {
+            //topk在bigger中，递归排序
+            return quickSort(bigger, k);
+        }else if (bigger.size()+equls.size()<k) {
+            //topk在small中 ,注意此时k有变化
+            return quickSort(small, k-bigger.size()-equls.size());
+        }
+        //topk就是flag
+        return flag;
+    }
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+class Solution1 {
+    /**
+     * 优先队列，小顶堆，堆顶是最小值
+     * 遍历数组所有元素，都放进小顶堆，超出k个就弹出堆顶最小值
+     * 最终剩下的就是较大的前k个，堆顶就是第k大
+     * 时间复杂度：O(nlogk)
+     * 空间复杂度：O(n)
+     */
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
         for (int i = 0; i < nums.length; i++) {
-            queue.offer(nums[i]);
-            if (queue.size()>k){
+            queue.add(nums[i]);
+            if (queue.size()>k) {
                 queue.poll();
             }
         }
         return queue.peek();
     }
 }
-//leetcode submit region end(Prohibit modification and deletion)
-
 
 }
