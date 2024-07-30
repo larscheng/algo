a28b228779e881ff8c7c0360f5a9a22ac90e64db
larscheng <larscheng@foxmail.com>
2024-07-30 17:10:24 +0800

    #197

---
+++ src/com/larscheng/www/leetcode/editor/cn/L92_ReverseLinkedListIi.java
@@ -0,0 +1,97 @@
+//给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链
+//表节点，返回 反转后的链表 。
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5], left = 2, right = 4
+//输出：[1,4,3,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [5], left = 1, right = 1
+//输出：[5]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目为 n 
+// 1 <= n <= 500 
+// -500 <= Node.val <= 500 
+// 1 <= left <= right <= n 
+// 
+//
+// 
+//
+// 进阶： 你可以使用一趟扫描完成反转吗？ 
+//
+// Related Topics 链表 👍 1809 👎 0
+
+package com.larscheng.www.leetcode.editor.cn;
+
+
+public class L92_ReverseLinkedListIi{
+      
+  public static void main(String[] args) {
+       Solution solution = new L92_ReverseLinkedListIi().new Solution();
+       ListNode a = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(6))))));
+       solution.reverseBetween(a,2,5);
+  }
+
+  public static class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        //找到左范围节点
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            //pre：待反转区第一个节点的前一个节点
+            //cur：待反转区第一个节点
+            //next:cur的下一个节点
+            //....[pre][cur][next]....
+            //pre->next,next->cur,cur->next.next
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
